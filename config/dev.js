78c5bf34b6fc852fedd69457fb5abc9660dbb0a4
vikash kumar <vikashkumar24041997@gmail.com>
2026-01-24 18:43:02 +0530

    mongo setup

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI:
+    'mongodb+srv://vikashkumar24041997:UwswORRUH164UIaH@cluster0.atpsuwu.mongodb.net/blog_adv_app',
   cookieKey: '123123123',
 };
