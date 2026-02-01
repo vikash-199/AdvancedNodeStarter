cffbed5c8a889a398b233c02a0367fe66ebc6ed7
vikash kumar <vikashkumar24041997@gmail.com>
2026-02-01 23:33:41 +0530

    header testing

---
+++ tests/header.test.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+
+test('Adds two numbers', () => {
+  const sum = 1 + 2;
+
+  expect(sum).toEqual(3);
+});
+
+test('Wecan lunch a browser', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto('localhost:3000');
+
+  const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
+});
