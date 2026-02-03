d61522256802ce05828c95f8a2820d909a15db70
vikash kumar <vikashkumar24041997@gmail.com>
2026-02-03 23:28:13 +0530

    testing using jest

---
+++ tests/logo.test.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+  });
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+test('the header has a correct text', async () => {
+  text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
+});
